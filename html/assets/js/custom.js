97facafa24a4529d5ca81bca2db7b7c3fe874b10
Sudipta Mallick <sudiptamallick@Sudiptas-MacBook-Pro.local>
2020-05-23 21:18:46 +0530

    Added changes.

--- html/assets/js/custom.js
+++ html/assets/js/custom.js
@@ -1,7 +1,7 @@
 // custom js page
 
 
-// headroom
+// headroom sticky top bar
 // grab an element
 var myElement = document.querySelector("section.o-brand");
 // construct an instance of Headroom, passing the element
@@ -11,5 +11,21 @@ var headroom  = new Headroom(myElement, {
 });
 // initialise
 headroom.init();
+
+
+// scroll to top
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $('#scroll').fadeIn(2000);
+        }else{
+            $('#scroll').fadeOut(2000);
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 2000);
+        return false;
+    });
+});
 
 
